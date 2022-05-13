8561da295aa039ee0a6d30b1c07ffbbf14ac316f
Guilherme Marques <jgmarques89@gmail.com>
2022-05-13 12:58:50 -0300

    criando o exercicio 3 do dia 1 do bloco 4

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-03.js
@@ -0,0 +1,11 @@
+const a = 56;
+const b = 57;
+const c = 55;
+
+if (a > b && a > c){
+    console.log(`O maior valor é a variavel A - ${a}`);
+} else if ( b > a && b > c){
+    console.log(`O maior valor é a variavel B - ${b}`);
+} else {
+    console.log(`O maior valor é a variavel C - ${c}`);
+}
