a5fee2632600cf0d88b1e4e5f70cefb08152b55c
Guilherme Marques <jgmarques89@gmail.com>
2022-05-15 23:13:08 -0300

    criando o exercicio 01 a 09 do dia 02 do bloco 04

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicio-01-a-09.js
@@ -0,0 +1,83 @@
+// Leia atentamente os enunciados e faça o que se pede! Iremos utilizar esse array para realizar os exercícios do 1 ao 7:
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// 1. Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log();
+
+/* for(let index = 0; index < numbers.length; index += 1 ){
+    console.log(numbers[index]);
+} */
+
+// 2 . Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+
+/* let num = 0;
+
+for(let i = 0; i < numbers.length; i += 1 ){
+   num += numbers[i]
+}
+
+console.log(num); */
+
+// 3. Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+
+/* let num = 0;
+
+for( let i= 0; i < numbers.length; i += 1){
+    num += numbers[i]
+}
+
+console.log(num / numbers.length ); */
+
+// 4. Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+
+/* let num = 0;
+
+for( let i= 0; i < numbers.length; i += 1){
+    num += numbers[i]
+}
+
+let resultadoTotal = num / numbers.length 
+
+if (resultadoTotal > 20 ){
+    console.log('valor maior que 20');
+} else {console.log('Valor menor ou igual a 20')}; */
+
+// 5. Utilizando for, descubra qual o maior valor contido no array e imprima-o;
+
+
+
+// 6. Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+
+
+
+// 7. Utilizando for, descubra qual o menor valor contido no array e imprima-o;
+
+
+
+// 8. Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
+
+/* let array = [] 
+let num = 25
+
+for(index = 1; index <= num; index +=1 ){
+
+    array.push(index)
+}   
+
+console.log(array); */ 
+
+// 9. Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2.
+
+/* let array = [] 
+let num = 25
+
+for(index = 1; index <= num; index +=1 ){
+
+    array.push(index / 2)
+}   
+
+console.log(array); */
+
+
+
+
